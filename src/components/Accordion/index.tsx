1a191c2fd6c25d3846088652669bc6fde55dd762
WandersonBarradas <wandersonbarradas07@gmail.com>
2023-10-24 20:48:03 -0300

    - Corrigido bug do Accordion não atualizar o height conforme elementos.

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./accordion.module.css";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import { SchemaFormSale } from "@/zod-schemas/schemaFormSale";
+import { useFormContext } from "react-hook-form";
 
 type Props = {
     title: string;
@@ -9,19 +11,27 @@ type Props = {
 };
 
 export const Accordion = ({ title, children, classes }: Props) => {
+    const {
+        formState: { errors },
+    } = useFormContext<SchemaFormSale>();
     const [showAccordion, setShowAccordion] = useState(true);
+
+    useEffect(() => {
+        if (errors) {
+            const body = document.getElementById("body" + title) as HTMLElement;
+            body.style.maxHeight = body.scrollHeight + "px";
+        }
+    }, [errors]);
 
     const handleClickAccordion = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setShowAccordion(!showAccordion);
-
         const body = document.getElementById("body" + title) as HTMLElement;
-        if (body.classList.contains("height-auto")) {
-            body.style.height = `${body.scrollHeight}px`;
-            body.classList.remove("height-auto");
+
+        if (showAccordion) {
+            body.style.maxHeight = "0";
         } else {
-            body.style.height = "0";
-            body.classList.add("height-auto");
+            body.style.maxHeight = body.scrollHeight + "px";
         }
     };
 
